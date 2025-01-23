81e4c79198457eae9a6b1542c082bb3b2a890523
purushothaman.d <your_email@example.com>
2025-01-23 23:21:21 +0530

    somthing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@
  * Sums three numbers
  * Sums three numbers together
  * 
+ * #!/bin/sh
+
+# Run the Python script after the commit
+python3 /home/finstein-emp/Music/TestingAutomation/gitstatus.py
+if [ $? -ne 0 ]; then
+  echo "Python script execution failed. Aborting post-commit process."
+  exit 1
+fi
+
+# Add additional actions you want to perform after commit, if needed
+
  * @module
  * @param {number} a - First number
  * @param {number} b - Second number
